2d0b39d316944bd4c5f85ed0d203d3de977c5359
Matheus Betim <mbetim47@gmail.com>
2022-03-18 11:51:47 -0300

    feat(websocket): set up the basic functions

---
+++ packages/server/src/events/events.gateway.ts
@@ -0,0 +1,43 @@
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { DefaultEventsMap } from 'socket.io/dist/typed-events';
+
+@WebSocketGateway()
+export class EventsGateway
+  implements
+    OnGatewayInit<Server>,
+    OnGatewayConnection<Socket>,
+    OnGatewayDisconnect<Socket>
+{
+  users = new Map<string, Socket>();
+
+  afterInit(
+    server: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>,
+  ) {
+    server.use((socket, next) => {
+      const { username } = socket.handshake.auth;
+      if (!username) return next(new Error('Username is required'));
+
+      if (this.users.has(username))
+        return next(new Error('Username is already taken'));
+
+      this.users.set(username, socket);
+      next();
+    });
+  }
+
+  handleConnection(client: Socket) {
+    console.log('Client connected', client.handshake.auth.username);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log('Client disconnected: ', client.id);
+
+    this.users.delete(client.handshake.auth.username);
+  }
+}
