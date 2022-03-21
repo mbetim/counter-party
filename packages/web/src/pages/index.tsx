77c16a95c556c4ffe4ddc65e550bc6f257edb93c
Matheus Betim <mbetim47@gmail.com>
2022-03-21 14:47:39 -0300

    feat: scratch the login page

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -1,16 +1,50 @@
-import { Typography } from "@mui/material";
+import { Button, Grid, Typography } from "@mui/material";
 import type { GetServerSideProps, NextPage } from "next";
 import { parseCookies } from "nookies";
+import { useEffect } from "react";
 import { PageContainer } from "../components/PageContainer";
+import { socket } from "../utils/socket";
 
 interface Props {
   username: string;
 }
 
 const Home: NextPage<Props> = ({ username }) => {
+  useEffect(() => {
+    if (socket.connected) return;
+
+    socket.auth = { username };
+    socket.connect();
+
+    socket.once("connect_error", () => {
+      // TODO: Handle this error
+      console.log("Failed to connect to socket");
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, [username]);
+
   return (
     <PageContainer title="Home">
-      <Typography variant="h3">Hello, {username}</Typography>
+      <Typography variant="h3" align="center">
+        Hello, {username}
+      </Typography>
+
+      <Grid container spacing={2} sx={{ mt: 2 }}>
+        <Grid item xs={12} md={6}>
+          <Button variant="outlined" fullWidth>
+            Create a party
+          </Button>
+        </Grid>
+
+        <Grid item xs={12} md={6}>
+          <Button variant="outlined" fullWidth>
+            Join a party
+          </Button>
+        </Grid>
+      </Grid>
     </PageContainer>
   );
 };
