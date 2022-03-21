73c69408cfb95fc5c7c084782c9b1dd607b7cdbf
Matheus Betim <mbetim47@gmail.com>
2022-03-21 14:11:05 -0300

    feat: change the mui theme

--- packages/web/src/utils/theme.ts
+++ packages/web/src/utils/theme.ts
@@ -1,3 +1,17 @@
 import { createTheme } from "@mui/material";
 
-export const theme = createTheme();
+export const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#51E5FF",
+    },
+    background: {
+      default: "#2274A5",
+    },
+    text: {
+      primary: "#FFFFFF",
+      secondary: "#EAEAEA",
+    },
+  },
+});
