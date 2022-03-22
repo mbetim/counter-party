34de7908cc9272289219da1cd8190b5052dbf1bb
Matheus Betim <mbetim47@gmail.com>
2022-03-22 09:01:05 -0300

    feat: create a hook to manipulate the dialogs

---
+++ packages/web/src/hooks/useDialog.ts
@@ -0,0 +1,36 @@
+import { useCallback, useState } from "react";
+
+interface UseDialogProps<T> {
+  open: boolean;
+  data?: T;
+}
+
+export interface UseDialogReturn<T> {
+  isOpen: boolean;
+  data: T | undefined;
+  open: (newData?: T) => void;
+  close: (newData?: T) => void;
+  toggle: (newData?: T) => void;
+}
+
+export function useDialog<T>(initialState?: UseDialogProps<T>): UseDialogReturn<T> {
+  const [isOpen, setIsOpen] = useState(initialState?.open || false);
+  const [data, setData] = useState<T | undefined>(initialState?.data || undefined);
+
+  const open = useCallback((newData?: T) => {
+    setData(newData);
+    setIsOpen(true);
+  }, []);
+
+  const close = useCallback((newData?: T) => {
+    setData(newData);
+    setIsOpen(false);
+  }, []);
+
+  const toggle = useCallback((newData?: T) => {
+    setData(newData);
+    setIsOpen((prev) => !prev);
+  }, []);
+
+  return { isOpen, data, open, close, toggle };
+}
